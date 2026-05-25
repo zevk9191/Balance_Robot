56bea1efadcd6ad463cb626739eaa2074e46c73c
Zakhar <zevk9191@gmail.com>
2026-05-25 21:48:42 +0300

    create project

---
+++ Core/Inc/altMain.h
@@ -0,0 +1,24 @@
+/*
+ * altMain.h
+ *
+ *  Created on: May 13, 2026
+ *      Author: zakharbihun
+ */
+
+#ifndef INC_ALTMAIN_H_
+#define INC_ALTMAIN_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void setup();
+void loop();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#endif /* INC_ALTMAIN_H_ */
---
+++ Core/Src/altMain.cpp
@@ -0,0 +1,22 @@
+/*
+ * altMain.cpp
+ *
+ *  Created on: May 13, 2026
+ *      Author: zakharbihun
+ */
+
+#include "altMain.h"
+#include "main.h"
+
+void setup() {
+
+}
+
+void loop() {
+
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, static_cast<GPIO_PinState>(1));
+//	  HAL_Delay(500);
+//	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
+//	  HAL_Delay(500);
+}
+
