ce25514089366c55146737a8b85fa3ab6ab94c8b
Zakhar Bihun <zevk9191@gmail.com>
2026-05-26 20:59:34 +0300

    test  commit

--- Core/Src/altMain.cpp
+++ Core/Src/altMain.cpp
@@ -15,7 +15,7 @@ void setup() {
 void loop() {
 
 	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, static_cast<GPIO_PinState>(1));
-//	  HAL_Delay(500);
+	  HAL_Delay(500);
 //	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
 //	  HAL_Delay(500);
 }
